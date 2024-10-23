a0634f24a7765d947dd4058cdcfbe68cbbf54b34
Novice <sharifmugisha06@gmail.com>
2024-10-23 12:35:53 +0200

    impelementing a command line interface

--- src/kernel/c/main.c
+++
@@ -1,6 +0,0 @@
-#include "../include/stdint.h"
-#include "../include/stdio.h"
-
-void _cdecl cstart_(){
-	printf("hello from C!, %d , %u , %x , %s, %lld", -333, 9979, 0xffac, "hello", -100000ll);
-}
